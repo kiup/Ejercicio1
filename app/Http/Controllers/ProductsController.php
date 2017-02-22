6053d8dcf7c2aa54d72c8e7e294fb457fc5630f0
kiup <milka.cabrilla@gmail.com>
2017-02-22 17:32:05 -0600

    HASTA AQUÍ LLEGUE ANTES DE LA CLASE PD: AGUILETA ES EL MEJOR MAESTRO DEL MUNDO MUNDIAL

---
+++ app/Http/Controllers/ProductsController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use App\Seller;
+use Illuminate\Http\Request;
+use Response;
+use App\Label;
+
+
+class ProductsController extends Controller
+{
+
+public function index_label()
+{
+  return Response::json(Label::all());
+}
+
+  public function index()
+  {
+    $array_product = Product::all();
+    $array_total = array();
+    foreach ($array_product as $i => $product) {
+      $array_for_product = array();
+      $array_for_product[] = $product;
+      $array_for_product[] = $product->seller;
+      $array_for_product[] = $product->label;
+
+      $array_total[] = $array_for_product;
+    }
+    return Response::json($array_total);
+  }
+
+  public function show($product)
+  {
+    $productInstance = Product::findOrFail($product);
+
+    $array_for_product = array();
+    $array_for_product[] = $productInstance;
+    $array_for_product[] = $productInstance->seller;
+    $array_for_product[] = $productInstance->label;
+
+    return Response::json($array_for_product);
+  }
+
+  public function store(Request $request, Seller $seller)
+  {
+    $attributes  = $request->all();
+    $product = new Product($attributes);
+    $seller->product()->save($product);
+    return Response::json($product);
+  }
+
+  public function update(Request $request, Product $product)
+  {
+    $attributes = $request->all();
+    $product -> update($attributes);//actualiza la información
+    return $product;
+  }
+
+  public function destroy(Product $product)
+  {
+    $product->delete();
+    return Response::json([],200);
+  }
+
+  public function store_review(Request $request, Product $product)
+  {
+    $attributes  = $request->all();
+    $review = new Review($attributes);
+    $product->review()->save($review);
+    return Response::json($review);
+  }
+
+  public function index_review(Product $product)
+  {
+    $reviews = $product->review->all();
+    return Response::json($reviews);
+  }
+
+  public function destroy_review(Request $request, Product $product, Review $review)
+  {
+    $review->delete();
+    return Response::json([],200);
+  }
+}
